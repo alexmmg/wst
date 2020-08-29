a06eb7295f7108cc5b2ca2c37c6cd65f0322625c
mmg.prokazyuk.o@gmail.com <mmg.prokazyuk.o@gmail.com>
2020-08-29 18:41:02 +0300

    header finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Header from "./Header/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    return (
+        <Container fluid className="m-0 p-0">
+            <Header />
+            <Row className="m-0 h-100">
+                <Col xs={3}>Sidebar</Col>
+                <Col>Main</Col>
+            </Row>
+            <Card>
+                <Card.Footer className="fixed-bottom">
+                    footer
+                </Card.Footer>
+            </Card>
+        </Container>
+    )
+};
 
 export default App;
---
+++ src/Header/Header.jsx
@@ -0,0 +1,41 @@
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import React from "react";
+import mainLogo from "../assets/img/header/main_logo.svg"
+import Row from "react-bootstrap/Row";
+
+const Header = () => {
+
+    const showInConsole = () => {
+        let chessBoard = "";
+        const n = 8;
+        for (let a = 0; a < n; a++) {
+            for (let b = 0; b < n; b++) {
+                chessBoard += (a % 2) == (b % 2) ? " " : "#";
+            }
+            chessBoard += "\n";
+        }
+        console.log(chessBoard);
+    };
+
+    return (
+        <div className="header">
+            <Card>
+                <Card.Header className="m-0">
+                    <Row className="justify-content-between">
+                        <Col xs={2}>
+                            <img src={mainLogo} alt="Main Logo" className="mainLogo"/>
+                        </Col>
+                        <Col xs={1} className="d-flex">
+                            <div className="align-self-center">
+                                <button className="btn btn-primary my-2" onClick={showInConsole}>PUSH</button>
+                            </div>
+                        </Col>
+                    </Row>
+                </Card.Header>
+            </Card>
+        </div>
+    )
+};
+
+export default Header;
